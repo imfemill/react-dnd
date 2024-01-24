51b183c73c09b669d9eda2e883d88fb5d135d1c7
femil.p <femil.p@webcodegenie.com>
2024-01-24 15:34:35 +0530

    update add task buttons

--- src/component/AddTask.jsx
+++ src/component/AddTask.jsx
@@ -45,17 +45,17 @@ const AddTask = ({ addContent, title, setIsInputVisible }) => {
                 placeholder="Enter new task"
                 value={newTask}
                 onChange={(e) => setNewTask(e.target.value)}
-                className="border p-2 mb-2 w-full rounded-md ring-2 max-h-40 min-h-11 focus-visible:outline-none ring-blue-400"
+                className="border p-2 mb-2 w-full rounded-md ring-2 max-h-40 min-h-11 focus-visible:outline-none ring-slate-700"
             />
             <button
                 onClick={() => handleAddTask(title)}
-                className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+                className="inline-block px-6 py-2.5 mx-5 mb-0 ml-auto font-bold text-right text-white uppercase align-middle transition-all border-0 rounded-lg cursor-pointer hover:scale-102 active:opacity-85 hover:shadow-soft-xs dark:bg-gradient-to-tl dark:from-slate-850 dark:to-gray-850 bg-gradient-to-tl from-gray-900 to-slate-800 leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25"
             >
                 Add
             </button>
             <button
                 onClick={handleCancelClick}
-                className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer ml-2"
+                className="inline-block px-6 py-2.5 mx-5 mb-0 ml-auto font-bold text-right text-gray-900 uppercase align-middle transition-all border-0 rounded-lg cursor-pointer hover:scale-102 active:opacity-85 hover:shadow-soft-xs dark:bg-gradient-to-tl bg-gradient-to-tl from-gray-50 to-slate-100 leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25"
             >
                 Cancel
             </button>
